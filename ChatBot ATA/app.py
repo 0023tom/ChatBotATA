cb93d0b8f24b350b34bd7df51d0fa6e1a845edf3
0023tom <164098190+0023tom@users.noreply.github.com>
2024-05-01 18:48:41 +0800

    Update app.py

--- ChatBot ATA/app.py
+++ ChatBot ATA/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template('index.html')
 
 @app.route("/get", methods = ["GET", "POST"])
 def chat():
